3d5761b42d7be63e7b2151ca7f0c5bdc4491bfbf
AJC <ajc212@naver.com>
2021-07-14 00:45:07 +0900

    FirstUpload

---
+++ src/main/java/com/three/dog/BayApplication.java
@@ -0,0 +1,13 @@
+package com.three.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayApplication.class, args);
+	}
+
+}
