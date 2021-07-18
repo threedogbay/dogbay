47268335753ed86f61d854a8924a11cbcaa528d1
yizi <yizi@localhost>
2021-07-18 23:36:47 +0900

    #21.07.18 #html

--- src/main/java/com/three/dog/MainController.java
+++ src/main/java/com/three/dog/MainController.java
@@ -2,6 +2,8 @@ package com.three.dog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +16,18 @@ public class MainController {
 		
 		return "index";
 	}
+	
+	@GetMapping(value="/{path1}")
+	public String path1(@PathVariable("path1") String path1
+			) {
+		return path1;
+	}
+	
+	@GetMapping(value="/{path1}/{path2}")
+	public String path2(@PathVariable("path1") String path1,
+			@PathVariable("path2") String path2
+			) {
+		return path1 + "/" + path2;
+	}
+	
 }
